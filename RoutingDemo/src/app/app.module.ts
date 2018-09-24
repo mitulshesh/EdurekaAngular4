2ea86329577067ff5f6e09c46d24ddbe03859812
Mitul <mitul.shesh28@gmail.com>
2018-09-24 10:06:39 +0530

    RoutingDemo updates

---
+++ RoutingDemo/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+
+//define the routes
+const ROUTES: Routes=[
+{ path:'', component : HomeComponent},
+{ path:'blog', component : BlogComponent},
+{ path:'about', component : AboutComponent},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    //Register the routes
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
